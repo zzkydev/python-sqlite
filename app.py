cafab5d8d26f79616cc33cb9aed8afdcd5ddc68c
zaki <103634066+zzkydev@users.noreply.github.com>
2025-11-22 20:26:37 +0700

    false debug for production

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sqlalchemy import text
 import sqlite3
 from functools import wraps
 import html
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
@@ -150,5 +151,5 @@ def edit_student(id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
 
